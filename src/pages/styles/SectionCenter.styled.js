049ef6ab46281ef264a1397a50597e1b46dbadaf
ksrinreddy <ksrinreddy@gmail.com>
2023-06-23 13:51:16 +0530

    add some basic styles to app

---
+++ src/pages/styles/SectionCenter.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const XCenter90 = styled.section`
+  width: 90vw;
+  max-width: calc(1024 * 0.0625rem);
+  margin-inline: auto;
+`;
+export const XCenterFull = styled.section`
+  width: 100vw;
+  max-width: calc(1024 * 0.0625rem);
+  margin-inline: auto;
+`;
